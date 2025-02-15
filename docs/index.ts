c370d54be4a7c0ebc479b9efc94316413a2ee760
Cosmo <gorcadaniel252@gmail.com>
2025-02-15 16:53:36 +0200

    changes

---
+++ docs/index.ts
@@ -0,0 +1,38 @@
+const url = "http://localhost:3000/jokes";
+const button = document.getElementById("submit") as HTMLButtonElement;
+const like =   document.getElementById("like")  as HTMLButtonElement;
+const dislike =   document.getElementById("dislike") as HTMLButtonElement;
+
+
+async function main() {
+    try{
+        const response = await fetch(url);
+        const data:any = await response.json();
+
+        let jokes = data.map(x => ({
+            id: x.id,
+            joke: x.joke,
+            punchine: x.punchline,
+            likes: x.likes,
+            dislikes: x.dislikes
+        }));
+
+        console.log(jokes);
+
+        for (let x in jokes){
+            console.log(x);
+        }
+
+        console.log(jokes[0].id);
+        
+        console.log(jokes);
+
+
+
+        
+    }catch(error){
+        console.error(error);
+    }
+
+
+}
